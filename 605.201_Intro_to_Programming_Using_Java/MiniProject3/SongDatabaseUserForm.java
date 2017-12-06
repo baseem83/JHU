8e2284c3ab939fe84e2358f57cde2c2753a11526
baseem83 <baseembu@gmail.com>
2017-12-05 21:20:55 -0500

    Control form state

--- 605.201_Intro_to_Programming_Using_Java/MiniProject3/SongDatabaseUserForm.java
+++ 605.201_Intro_to_Programming_Using_Java/MiniProject3/SongDatabaseUserForm.java
@@ -8,12 +8,31 @@ import javafx.collections.*;
 
 public class SongDatabaseUserForm extends Application
 {
+    private SongDataContext dataContext;
+    private Button addButton = new Button("Add");
+    private Button editButton;
+    private Button deleteButton;
+    private Button acceptButton;
+    private Button cancelButton;
+    private Button exitButton;
+    private ComboBox<String> songCombo;
+    private TextField itemCodeText;
+    private TextField descText;
+    private TextField artistText;
+    private TextField albumText;
+    private TextField priceText;
+    private Label statusLabel;
+
+    private Status formStatus;
+
     @Override
     public void start(Stage primaryStage)
     {
         primaryStage.setTitle("Songs Libaray");
         // FlowPane rootNode = new FlowPane(Orientation.VERTICAL, 5, 5);
         // rootNode.setAlignment(Pos.TOP_CENTER);
+
+        System.out.println(this.getParameters().getUnnamed().get(0));
 
         GridPane entryGrid = new GridPane();
         // entryGrid.setPrefSize(300, 450);
@@ -30,26 +49,26 @@ public class SongDatabaseUserForm extends Application
         entryGrid.getColumnConstraints().addAll(column1, column2);
 
         Label titleLabel = new Label("Title");
-        ComboBox<String> songCombo = new ComboBox<String>();
+        songCombo = new ComboBox<String>();
         // songCombo.setEditable(true);
         // songCombo.setPromptText("Enter something here");
 
         Label itemCodeLabel = new Label("Item Code");
-        TextField itemCodeText = new TextField();
+        itemCodeText = new TextField();
         itemCodeText.setPrefWidth(100);
         GridPane.setFillWidth(itemCodeText, false);
 
         Label descLabel = new Label("Description");
-        TextField descText = new TextField();
+        descText = new TextField();
 
         Label artistLabel = new Label("Artist");
-        TextField artistText = new TextField();
+        artistText = new TextField();
 
         Label albumLabel = new Label("Album");
-        TextField albumText = new TextField();
+        albumText = new TextField();
 
         Label priceLabel = new Label("Price");
-        TextField priceText = new TextField();
+        priceText = new TextField();
         priceText.setPrefWidth(100);
         GridPane.setFillWidth(priceText, false);
 
@@ -79,12 +98,12 @@ public class SongDatabaseUserForm extends Application
 
         FlowPane buttonPane = new FlowPane(20, 10);
 
-        Button addButton = new Button("Add");
-        Button editButton = new Button("Edit");
-        Button deleteButton = new Button("Delete");
-        Button acceptButton = new Button("Accept");
-        Button cancelButton = new Button("Cancel");
-        Button exitButton = new Button("Exit");
+        addButton = new Button("Add");
+        editButton = new Button("Edit");
+        deleteButton = new Button("Delete");
+        acceptButton = new Button("Accept");
+        cancelButton = new Button("Cancel");
+        exitButton = new Button("Exit");
 
         addButton.setPrefWidth(70);
         editButton.setPrefWidth(70);
@@ -99,14 +118,108 @@ public class SongDatabaseUserForm extends Application
 
         entryGrid.add(buttonPane, 0, 6, 2, 1);
 
+        statusLabel = new Label();
+
+        GridPane.setConstraints(statusLabel, 0, 7);
+        entryGrid.getChildren().add(statusLabel);
+
         Scene songScene = new Scene(entryGrid, 500, 350);
+
+        setStatus(Status.VIEW);
 
         primaryStage.setScene(songScene);
         primaryStage.show();
     }
 
+    private void setStatus(Status status)
+    {
+        formStatus = status;
+        statusLabel.setText("Status: " + formStatus.name());
+
+
+        
+        switch(status)
+        {
+            case ADD:
+                songCombo.setDisable(false);
+                itemCodeText.setDisable(false);
+                descText.setDisable(false);
+                artistText.setDisable(false);
+                albumText.setDisable(false);
+                priceText.setDisable(false);
+
+                addButton.setDisable(false);
+                editButton.setDisable(true);
+                deleteButton.setDisable(true);
+                acceptButton.setDisable(true);
+                cancelButton.setDisable(true);
+                exitButton.setDisable(false);
+
+                songCombo.setEditable(true);
+
+                break;
+            case EDIT:
+                songCombo.setDisable(true);
+                itemCodeText.setDisable(true);
+                descText.setDisable(false);
+                artistText.setDisable(false);
+                albumText.setDisable(false);
+                priceText.setDisable(false);
+
+                addButton.setDisable(true);
+                editButton.setDisable(true);
+                deleteButton.setDisable(true);
+                acceptButton.setDisable(false);
+                cancelButton.setDisable(false);
+                exitButton.setDisable(true);
+
+                break;
+            case DELETE:
+                songCombo.setDisable(true);
+                itemCodeText.setDisable(true);
+                descText.setDisable(true);
+                artistText.setDisable(true);
+                albumText.setDisable(true);
+                priceText.setDisable(true);
+
+                addButton.setDisable(true);
+                editButton.setDisable(true);
+                deleteButton.setDisable(true);
+                acceptButton.setDisable(false);
+                cancelButton.setDisable(false);
+                exitButton.setDisable(true);
+
+                break;
+            case VIEW:
+                songCombo.setDisable(false);
+                itemCodeText.setDisable(true);
+                descText.setDisable(true);
+                artistText.setDisable(true);
+                albumText.setDisable(true);
+                priceText.setDisable(true);
+
+                addButton.setDisable(false);
+                editButton.setDisable(false);
+                deleteButton.setDisable(false);
+                acceptButton.setDisable(true);
+                cancelButton.setDisable(true);
+                exitButton.setDisable(false);
+
+                break;
+
+        }
+    }
+
     public static void main(String [] args)
     {
         launch(args);
+    }
+
+    enum Status
+    {
+        ADD,
+        EDIT,
+        DELETE,
+        VIEW
     }
 }
