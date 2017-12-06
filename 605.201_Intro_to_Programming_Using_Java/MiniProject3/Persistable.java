81828f248a5a3c929f9d5be8756175865bf48b17
Baseem Astiphan <baseembu@gmail.com>
2017-12-06 15:25:32 -0500

    Add Javadocs for Persistable inteface

--- 605.201_Intro_to_Programming_Using_Java/MiniProject3/Persistable.java
+++ 605.201_Intro_to_Programming_Using_Java/MiniProject3/Persistable.java
@@ -1,3 +1,9 @@
+/**
+    * Interface definining methods needed for persistence to a text file database
+    *
+    * @author Baseem Astiphan
+    * @version 1.0.0.1
+*/
 public interface Persistable<T, O>
 {
     T getKeyField();
